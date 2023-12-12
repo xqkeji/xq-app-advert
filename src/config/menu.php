634a367d794f3212370595207a683f2c1b3017e8
xqkeji <xqkeji@foxmail.com>
2023-12-12 19:33:29 +0800

    更新菜单格式

--- src/config/menu.php
+++ src/config/menu.php
@@ -1,20 +1,23 @@
 <?php
-return [	
-	'title'=>'广告管理',
-	'children'=>[
-		[
-			'url'=>'position/admin',
-			'title'=>'广告位管理',
-			'icon'=>'bi bi-cast',
+return [
+	'admin'=>[
+		'title'=>'广告管理',
+		'children'=>[
+			[
+				'url'=>'position/admin',
+				'title'=>'广告位管理',
+				'icon'=>'bi bi-cast',
+			],
+			[
+				'url'=>'position/submenu',
+				'title'=>'广告管理',
+				'icon'=>'bi bi-broadcast',
+				'submenu'=>true,
+			],
+			
+			
 		],
-		[
-			'url'=>'position/submenu',
-			'title'=>'广告管理',
-			'icon'=>'bi bi-broadcast',
-			'submenu'=>true,
-		],
-		
-		
-	],
+	]
+	
 	
 ];
